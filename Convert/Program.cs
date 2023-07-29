89024ed5893e5d0fb47e96fd17e689d4e517a747
rezaamiirii <102056120+rezaamiirii@users.noreply.github.com>
2023-07-29 19:30:16 +0330

    Add project files.

---
+++ Convert/Program.cs
@@ -0,0 +1,29 @@
+﻿using FellowOakDicom;
+using FellowOakDicom.Imaging;
+using FellowOakDicom.Imaging.Codec;
+using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = DicomFile.Open(@"2023_02_08_093904_RL_PELVIS.dcm");  
+         
+            var newFile = file.Clone(DicomTransferSyntax.ExplicitVRLittleEndian);
+
+            newFile.Save(@"outputsd.dcm");
+            
+        }
+       
+    }
+}
