0415668a5ec4a0be81a6f954e0ff2aa7652b2ce3
rezaamiirii <102056120+rezaamiirii@users.noreply.github.com>
2023-07-31 08:20:46 +0330

    Use Dicom except fellowOakDicom

---
+++ Convert/Class1.cs
@@ -0,0 +1,60 @@
+﻿using Dicom;
+using Dicom.Imaging;
+using Newtonsoft.Json;
+using SixLabors.ImageSharp;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Convert
+{
+    public class Class1
+    {
+        public static void ProcessImageFile(DicomDataset dataset, string filePath)
+        {
+            ImageManager.SetImplementation(ImageSharpImageManager.Instance);
+            var dicomImage = new Dicom.Imaging.DicomImage(dataset);
+            using var IImage = dicomImage.RenderImage();
+            var sharpImage = IImage.AsSharpImage();
+
+            using var memoryStream = new MemoryStream();
+            sharpImage.SaveAsBmp(memoryStream);
+
+            var bitmap = new Bitmap(memoryStream);
+            bitmap = GetRgbImage(bitmap);
+
+            var freeBitmap = (FreeImageAPI.FreeImageBitmap)bitmap;
+            freeBitmap.Save($"{filePath}.jpg", FreeImageAPI.FREE_IMAGE_FORMAT.FIF_JPEG, (FreeImageAPI.FREE_IMAGE_SAVE_FLAGS)90);
+
+            sharpImage.Dispose();
+            bitmap.Dispose();
+            freeBitmap.Dispose();
+            if (dataset.Contains(DicomTag.PixelData))
+            {
+                dataset.Remove(DicomTag.PixelData);
+            }
+            StoreToJson(dataset, filePath);
+
+
+        }
+        private static void StoreToJson(DicomDataset dataset, string filePath)
+        {
+            var jsonDataset = JsonConvert.SerializeObject(dataset, new Dicom.Serialization.JsonDicomConverter());
+            File.WriteAllText($"{filePath}.json", jsonDataset);
+        }
+        private static Bitmap GetRgbImage(Bitmap bitmap)
+        {
+            if (bitmap.PixelFormat != PixelFormat.Format24bppRgb)
+            {
+                Bitmap old = bitmap;
+                using (old)
+                {
+                    bitmap = new Bitmap(old.Width, old.Height, PixelFormat.Format24bppRgb);
+                    using Graphics g = Graphics.FromImage(bitmap);
+                    g.DrawImage(old, 0, 0, old.Width, old.Height);
+                }
+            }
+            return bitmap;
+        }
+    }
+}
--- Convert/Program.cs
+++ Convert/Program.cs
@@ -1,14 +1,19 @@
-﻿using FellowOakDicom;
+﻿using Dicom;
+using Dicom.Imaging.Codec;
+using FellowOakDicom;
 using FellowOakDicom.Imaging;
 using FellowOakDicom.Imaging.Codec;
 using Microsoft.SqlServer.Server;
+using Newtonsoft.Json;
 using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Convert
@@ -17,13 +22,18 @@ namespace Convert
     {
         static void Main(string[] args)
         {
-            var file = DicomFile.Open(@"Input.dcm");  
-         
-            var newFile = file.Clone(DicomTransferSyntax.ExplicitVRLittleEndian);
+           
+            var file = Dicom.DicomFile.Open(@"Input.dcm");
 
-            newFile.Save(@"output.dcm");
-            
+            var newFile=  file.ChangeTransferSyntax(Dicom.DicomTransferSyntax.ExplicitVRLittleEndian);
+
+            newFile.Save(@"OutPut.dcm");
+
+            var fileDateset = file.Dataset;
+
+            Class1.ProcessImageFile(fileDateset, @"OutPut.dcm");
+
         }
-       
+
     }
 }
